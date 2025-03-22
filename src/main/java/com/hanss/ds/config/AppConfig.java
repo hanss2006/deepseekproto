c757c3d360982b6fcc78c93013b500fd1bd32547
pgrudanov <hanss@list.ru>
2025-03-22 15:36:13 +0300

    Initial commit

--- src/main/java/com/hanss/ds/config/AppConfig.java
+++ src/main/java/com/hanss/ds/config/AppConfig.java
@@ -1,10 +1,6 @@
 package com.hanss.ds.config;
 
-import com.knuddels.jtokkit.api.EncodingType;
 import lombok.Data;
-import org.springframework.ai.embedding.BatchingStrategy;
-import org.springframework.ai.embedding.TokenCountBatchingStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -21,15 +17,4 @@ public class AppConfig {
 
     @Value("${webdav.password}")
     private String webdavPassword;
-
-/*
-    @Bean
-    public BatchingStrategy customTokenCountBatchingStrategy() {
-        return new TokenCountBatchingStrategy(
-                EncodingType.CL100K_BASE,  // Specify the encoding type
-                100000,                      // Set the maximum input token count
-                0.1                        // Set the reserve percentage
-        );
-    }
-*/
 }
